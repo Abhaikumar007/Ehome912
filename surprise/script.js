1fd244613ac0979c8f5fb0528e23f098a6ac8e72
Abhaikumar007 <72512310+Abhaikumar007@users.noreply.github.com>
2023-06-04 17:38:08 +0530

    Update script.js

--- surprise/script.js
+++ surprise/script.js
@@ -83,7 +83,7 @@ const shareButton = document.getElementById("share-button");
 shareButton.addEventListener("click", () => {
 
     // Redirect to WhatsApp with a pre-filled message
-    const text = "*🎉Edu Home Mega Gift Surprise* 😱🎊;                                                      Take Our Survey and claim your 100% guaranteed *GIFT*💯 worth ₹1000 and more.  https://myeduhome.netlify.app/surprise/index.html  \n```                                  Exciting News!💎 Eduhome is now at Muslim Street,Kottarakkara``` \nAdmission Open";
+    const text = "*Edu Home Mega Gift Surprise* ;                                                      Take Our Survey and claim your 100% guaranteed *GIFT* worth ₹1000 and more.  https://myeduhome.netlify.app/surprise/index.html  \n```                                  Exciting News!💎 Eduhome is now at Muslim Street,Kottarakkara``` \nAdmission Open";
     const phoneNumber = ""; // Replace with your phone number
     const whatsappLink = `https://api.whatsapp.com/send?text=${text}`;
    //const whatsappLink = `#`;
